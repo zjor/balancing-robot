6fa6b358f21e56f6f04c1ded85619d0fcafc789f
Sergey Royz <zjor.se@gmail.com>
2021-04-29 11:36:40 +0200

    send MPU data via UDP

--- src/stream_mpu_data.py
+++ src/stream_mpu_data.py
@@ -3,29 +3,29 @@
 import time
 import socket
 
-# from sensor.mpu import MPU
+from sensor.mpu import MPU
 from util.timed_task import TimedTask
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-pitch = 0.0
-roll = 0.0
 
 
 def run(now, dt):
-    global pitch, roll
-    pitch += 0.1
-    roll -= 0.1
+    global mpu
+    roll, pitch = mpu.get_roll_pitch_deg()
     message = bytes(f"{pitch} {roll}", "utf-8")
     server.sendto(message, ('<broadcast>', 37020))
-    print(f"{now}: {message}")
+    # print(f"{now}: {message}")
 
-
-tt = TimedTask(delay=0.1, run=run)
+mpu = MPU()
+tt = TimedTask(delay=0.05, run=run)
 if __name__ == "__main__":
+
+    print("Broadcasting MPU data...")
     try:
         while True:
             tt.loop()
     except KeyboardInterrupt:
         pass
+    print("Done")
