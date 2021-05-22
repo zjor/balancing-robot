fe70cc1f498e3a8eed03636fc6e5b8fa42c13a0b
Sergey Royz <zjor.se@gmail.com>
2021-05-22 16:43:08 +0200

    get back control code

--- esp32/balancing-robot/src/main.cpp
+++ esp32/balancing-robot/src/main.cpp
@@ -1,68 +1,36 @@
 #include <Arduino.h>
-
 #include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-#include <Wire.h>
-
 #include "imu/imu.h"
-#include "util/logger.h"
+#include "PID.h"
+#include "TimedTask.h"
 
-Logger logger(false);
 
-const float accel_offset[3] = {0.8864, -0.1769, -1.1098};
-const float gyro_offset[3] = {-0.0160, 0.0215, -0.0261};
+#define RIGHT_MOTOR_STEP_PIN  15
+#define RIGHT_MOTOR_DIR_PIN   32
+#define LEFT_MOTOR_STEP_PIN   17
+#define LEFT_MOTOR_DIR_PIN    21
+
+#define PPR       1600
+
+#define TICKS_PER_SECOND 200000
+
+#define SET_POINT 0.0
+
+
+const bool shouldCalibrate = false;
 
 Adafruit_MPU6050 mpu;
 IMU imu(&mpu, 0.95);
 
-roll_pitch_t getAccelRollPitch(float ax, float ay, float az) {
-  const float alpha = 0.2;
-  static float fx = ax;
-  static float fy = ay;
-  static float fz = az;
+PID pid(5.0, 10.0, 0.0, SET_POINT * DEG_TO_RAD);
+bool is_balancing = false;
 
-  fx = alpha * ax + (1.0 - alpha) * fx;
-  fy = alpha * ay + (1.0 - alpha) * fy;
-  fz = alpha * az + (1.0 - alpha) * fz;
+hw_timer_t * timer = NULL;
+volatile uint32_t ticksPerPulse = TICKS_PER_SECOND;
+volatile uint32_t currentTick = 0;
 
-  roll_pitch_t r;
-  // r.roll = atan2(-fy, fz);
-  r.roll = atan2(-fy, sqrt(fx * fx + fz * fz));
-  r.pitch = atan2(fx, sqrt(fy * fy + fz * fz));
-
-  return r;
-}
-
-roll_pitch_t getRawAccelRollPitch(float ax, float ay, float az) {
-  roll_pitch_t r;
-  // r.roll = atan2(-ay, az);
-  r.roll = atan2(-ay, sqrt(ax * ax + az * az));
-  r.pitch = atan2(ax, sqrt(ay * ay + az * az));
-
-  return r;
-}
-
-roll_pitch_t getGyroRollPitch(float gx, float gy, float gz, float roll_accel, float pitch_accel) {
-  static unsigned long ts = micros();
-  static float roll = roll_accel;
-  static float pitch = pitch_accel;
-
-  unsigned long now = micros();
-  float dt = 1e-6 * (now - ts);
-  ts = now;
-
-  roll += -gy * dt;
-  pitch += -gx * dt;
-
-  const float a = 0.91;
-  roll = a * roll + (1.0 - a) * roll_accel;
-  pitch = a * pitch + (1.0 - a) * pitch_accel;
-
-  roll_pitch_t r;
-  r.roll = roll;
-  r.pitch = pitch;
-  return r;
-}
+double angle;
+double velocity;
 
 void initMPU() {
   if (!mpu.begin()) {
@@ -78,33 +46,76 @@ void initMPU() {
 }
 
 void calibrate() {  
-
-  imu.setAccelOffset({ -1.5999, 0.4344, -1.7590 });
-  imu.setGyroOffset({ -0.0254, 0.0050, -0.0075 });
-  return;
-
-  Serial.println("Calibrating...");
-
-  tuple_t<vector3d_t, vector3d_t> offsets = imu.calibrate(2000, 10);
-
-  Serial.print("Acc offset: ");
-  Serial.print(offsets.a.x, 4);
-
-  Serial.print(", ");
-  Serial.print(offsets.a.y, 4);
-
-  Serial.print(", ");
-  Serial.println(offsets.a.z, 4);
-
-  Serial.print("Gyro offset: ");
-  Serial.print(offsets.b.x, 4);
-
-  Serial.print(", ");
-  Serial.print(offsets.b.y, 4);
-
-  Serial.print(", ");
-  Serial.println(offsets.b.z, 4);  
+  if (shouldCalibrate) {
+    Serial.println("Calibrating...");
+    tuple_t<vector3d_t, vector3d_t> offsets = imu.calibrate(2000, 10);
+    Serial.printf("Accel offset: %.4f, %.4f %.4f\n", offsets.a.x, offsets.a.y, offsets.a.z);
+    Serial.printf("Gyro offset: %.4f, %.4f %.4f\n", offsets.b.x, offsets.b.y, offsets.b.z);
+  } else {
+    imu.setAccelOffset({ -1.5999, 0.4344, -1.7590 });
+    imu.setGyroOffset({ -0.0254, 0.0050, -0.0075 });
+  }
 }
+
+int getTicksPerPulse(float velocity) {
+  if (abs(velocity) < 1e-3) {
+    return UINT32_MAX;
+  } else {
+    return (uint32_t)(2.0 * PI * TICKS_PER_SECOND / (abs(velocity) * PPR));
+  }  
+}
+
+void setVelocity(float velocity) {
+  digitalWrite(LEFT_MOTOR_DIR_PIN, velocity > 0 ? HIGH : LOW);
+  digitalWrite(RIGHT_MOTOR_DIR_PIN, velocity < 0 ? HIGH : LOW);
+  ticksPerPulse = getTicksPerPulse(velocity);
+}
+
+void IRAM_ATTR onTimer() {
+  if (currentTick >= ticksPerPulse) {
+    currentTick = 0;
+  }
+  if (currentTick == 0) {
+    digitalWrite(LEFT_MOTOR_STEP_PIN, HIGH);
+    digitalWrite(RIGHT_MOTOR_STEP_PIN, HIGH);
+  } else if (currentTick == 1) {
+    digitalWrite(LEFT_MOTOR_STEP_PIN, LOW);
+    digitalWrite(RIGHT_MOTOR_STEP_PIN, LOW);
+  }
+  currentTick++; 
+}
+
+void log(unsigned long now_millis, unsigned long dt_millis) {
+  Serial.printf("%.4f\t%.4f\n", 
+    angle, 
+    velocity);
+}
+
+TimedTask loggerTask(log, 50);
+
+void control(unsigned long now_millis, unsigned long dt_millis) {
+  float dt = dt_millis * 1e-3;
+
+  if (abs(angle - SET_POINT) < 5.0) {
+    is_balancing = true;
+  }
+
+  if (abs(angle - SET_POINT) > 50.0) {
+    is_balancing = false;
+    velocity = 0.0;
+    setVelocity(velocity);
+  }
+
+  if (!is_balancing) {
+    return;
+  }
+
+  float u = pid.getControl(angle * DEG_TO_RAD, dt);
+  velocity += u * dt;
+  setVelocity(velocity);  
+}
+
+TimedTask controlTask(control, 7);
 
 void setup(void) {
   Serial.begin(115200);
@@ -114,18 +125,14 @@ void setup(void) {
 }
 
 void loop() {
+  if (shouldCalibrate) {
+    return;
+  }
+  loggerTask.loop();
+  controlTask.loop();
   imu.update();
-  // roll_pitch_t g_rp = imu.getGyroRollPitch();
-  roll_pitch_t a_rp = imu.getAccelRollPitch();
+
   roll_pitch_t f_rp = imu.getFilteredRollPitch();
+  angle = f_rp.roll * RAD_TO_DEG;
 
-  Serial.printf("%.4f\t%.4f\t%.4f\t%.4f\n", 
-    // g_rp.roll * RAD_TO_DEG, 
-    // g_rp.pitch * RAD_TO_DEG,
-    a_rp.roll * RAD_TO_DEG, 
-    a_rp.pitch * RAD_TO_DEG,
-    f_rp.roll * RAD_TO_DEG, 
-    f_rp.pitch * RAD_TO_DEG);
-
-  delay(1);
 }
