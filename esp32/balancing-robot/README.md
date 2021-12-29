92010e3deffb31f7032b48f2d40423368d788605
Sergey Royz <zjor.se@gmail.com>
2021-12-29 13:10:43 +0100

    reading IMU

---
+++ esp32/balancing-robot/README.md
@@ -0,0 +1,10 @@
+# Balancing Robot ESP32 WROOM
+
+## Patching `SparkFun MPU-9250 Digital Motion Processing _DMP_ Arduino Library`
+
+1. Go to `src/util/inv_mpu.h`
+2. Add the following lines:
+```C
+#define min(X,Y) (((X) < (Y)) ? (X) : (Y))
+#define max(X,Y) (((X) > (Y)) ? (X) : (Y))
+```
--- esp32/balancing-robot/src/main.cpp
+++ esp32/balancing-robot/src/main.cpp
@@ -40,9 +40,9 @@
 
 #include "pid/PID.h"
 
-#define PIN_MPU_SCL 22
-#define PIN_MPU_SDA 21
-#define PIN_MPU_INT 19
+#define PIN_IMU_SCL 22
+#define PIN_IMU_SDA 21
+#define PIN_IMU_INT 19
 
 #define PIN_MOTOR_RIGHT_EN    15
 #define PIN_MOTOR_RIGHT_DIR   14
@@ -69,6 +69,59 @@
 #define WARMUP_DELAY_US (5000000UL)
 
 #define ANGLE_SET_POINT (2.0 * DEG_TO_RAD)
+
+#define LOG_IMU
+
+MPU9250_DMP imu;
+volatile bool dmpDataReady = false;
+float roll, pitch, yaw;
+
+void dmpISR() {
+  dmpDataReady = true;
+}
+
+void initIMU() {
+  pinMode(PIN_IMU_INT, INPUT_PULLUP);
+
+  if (imu.begin() != INV_SUCCESS) {
+    while (1) {
+      Serial.println("Failed to init IMU");
+      delay(5000);
+    }
+  }
+
+  imu.dmpBegin(DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_GYRO_CAL, 100);
+  imu.enableInterrupt();
+  imu.setIntLevel(INT_ACTIVE_LOW);
+  imu.setIntLatched(INT_LATCHED);
+
+  attachInterrupt(PIN_IMU_INT, dmpISR, FALLING);
+}
+
+void readIMU() {
+  if (dmpDataReady && imu.fifoAvailable()) {
+    if (imu.dmpUpdateFifo() == INV_SUCCESS) {
+      dmpDataReady = false;
+      imu.computeEulerAngles();
+      roll = imu.roll;
+      pitch = imu.pitch;
+      yaw = imu.yaw;
+    }
+  }
+}
+
+void logIMU() {
+  #ifdef LOG_IMU
+  static unsigned long lastTimestamp = millis();
+  unsigned long now = millis();
+  if (now - lastTimestamp > 50) {
+    Serial.print("r:"); Serial.print(roll); Serial.print("\t");
+    Serial.print("p:"); Serial.print(pitch); Serial.print("\t");
+    Serial.print("y:"); Serial.println(yaw);
+    lastTimestamp = now;
+  }
+  #endif
+}
 
 PID angle_pid(ANGLE_Kp, ANGLE_Kd, ANGLE_Ki, ANGLE_SET_POINT);
 PID velocity_pid(0.00, 0.0, 0.0, 0.0);
@@ -98,28 +151,24 @@ void IRAM_ATTR onTimer() {
 }
 
 
-
-
-
-
 void setup(void) {
-  setCpuFrequencyMhz(240);
+  // setCpuFrequencyMhz(240);
 
   Serial.begin(115200);
   Wire.begin();
   Wire.setClock(1000000UL);
 
-
-
+  // TODO: add comments to params
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
   timerAlarmWrite(timer, 5, true);
-  timerAlarmEnable(timer);  
+  timerAlarmEnable(timer);
+
+  initIMU();
 
 }
 
-
-
 void loop() {
-
+  readIMU();
+  logIMU();
 }
